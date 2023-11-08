ab9837da55b8b28df38c4522ebbe7bb1d4d5b72f
Dunuj <150252106+Dunuj@users.noreply.github.com>
2023-11-08 20:30:46 +0800

    Add files via upload

---
+++ lianai.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>单身狗</title>
+
+
+
+</head>
+
+<body>
+    <?php
+    if (isset($_GET['name'])) {
+        echo 'Hello单身狗 ' . $_GET['name'];
+    } else {
+        echo '请输入单身狗的姓名';
+    }
+    ?>
+    <a href=http://172.22.224.89/1/lianai.php>
+        <center><img src=单身狗.png></center>
+    </a>
+    <h2 align=center>开始你的恋爱之旅吧！</h2>
+</body>
+
+</html>
